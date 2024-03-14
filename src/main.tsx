5df78d79bcb54a288e615298af565d5686fc82c7
johnnyyork13 <johnnyyork13@hotmail.com>
2024-03-14 17:05:15 -0400

    update build

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
-
 import {Provider} from 'react-redux';
 import {store} from './state/store.ts';
 
